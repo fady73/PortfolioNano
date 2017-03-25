24b4a848fc014e11eb75ff463f8d301971b48396
fady khayrat <fadykhayrat@gmail.com>
2017-03-25 02:21:04 +0200

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+       dev: {
+         options: {
+          engine: 'gm',
+          sizes: [ {
+            width: 1600,
+            suffix: '_2x',
+            quality: 30
+          }, {
+            width: 800,
+            suffix: '_1x',
+            quality: 30
+          }]
+        },
+
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+		        cwd: 'images_src/',
+
+         dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: ['images_src/*.{gif,jpg,png}'],
+          dest: 'images/',
+          flatten: true,
+        }]
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
